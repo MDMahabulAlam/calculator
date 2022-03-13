a9bda25fa420920b25ba5748f1e84f1ba2f78ae1
MDMahabulAlam <101058063+MDMahabulAlam@users.noreply.github.com>
2022-03-13 13:25:39 +0600

    update

--- cal.php
+++ cal.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>PHP Calculator</title>
+	<title>PHP Calculator by Mahabul</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
